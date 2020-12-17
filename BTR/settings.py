758ff6c3f13a5cfddd662fc50e0e7c677e470bee
isk030 <luvak61@hotmail.com>
2020-12-17 21:31:12 +0100

    over 50% done til chapter 51

--- BTR/settings.py
+++ BTR/settings.py
@@ -32,12 +32,14 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize'
 ]
 
 MIDDLEWARE = [
