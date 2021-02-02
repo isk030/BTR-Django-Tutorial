9b8788057410ad0d6cb7b3fbb3323b375431f338
Iskender <iskender.dilaver@gmail.com>
2021-02-02 13:05:58 +0100

    heck

--- realtors/models.py
+++ realtors/models.py
@@ -3,7 +3,7 @@ from datetime import datetime
 # Create your models here.
 class Realtor(models.Model):
     name = models.CharField(max_length=100)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     description = models.TextField(blank=True)
     phone = models.CharField(max_length=20)
     email = models.CharField(max_length=50)
